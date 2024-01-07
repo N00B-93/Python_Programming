0db05c5bc1b6854325c6cfa01fbef490b2f0c14c
N00B-93 <omosheye007@gmail.com>
2024-01-07 07:18:18 -0800

    Add CompareCost.py, LearnAddition.py, LearnMultiplication.py source files.

---
+++ Selections/CompareCost.py
@@ -0,0 +1,19 @@
+"""
+    This is a program that prompts the user to enter the weight and price
+    of two different packages of rice , then compares the cost of two packages and displays the
+    one with the better price.
+"""
+
+# Reads in the weight and price of each package.
+weight1, price1 = eval(input("\nEnter the weight and price of the first package separated by comma: "))
+weight2, price2 = eval(input("\nEnter the weight and price of the second package separated by comma: "))
+
+# Calculates the pricePerKg of each package.
+pricePerKg1 = price1 / weight1
+pricePerKg2 = price2 / weight2
+
+# Displays the package with the better price based on the pricePerKg.
+if pricePerKg1 < pricePerKg2:
+    print("\nPackage1 has the better price.")
+else:
+    print("\nPackage2 has the better price.")
--- Selections/LearnMultiplication.py
+++ Selections/LearnMultiplication.py
@@ -2,7 +2,7 @@ from random import randint
 
 """
     This a program that generates two integers under 100 and
-    prompts the user to enter the sum of these two integers. The program then reports
+    prompts the user to enter the product of these two integers. The program then reports
     true if the answer is correct, false otherwise.
 """
 
@@ -11,10 +11,10 @@ number1 = randint(1, 100)
 number2 = randint(1, 100)
 
 # Calculates the sum of the numbers
-sumOfNumbers = number1 + number2
+sumOfNumbers = number1 * number2
 
 # Prompts the user to enter the sum of the numbers.
-answer = int(input(f"\nWhat is {number1} + {number2}? "))
+answer = int(input(f"\nWhat is {number1} * {number2}? "))
 
 # Informs the user if the solution he/she enters is right or wrong.
 if answer == sumOfNumbers:
