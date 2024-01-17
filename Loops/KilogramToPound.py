d6c53fa41e53e0cc80b8134135facebff51c8a03
N00B-93 <omosheye007@gmail.com>
2024-01-16 21:33:44 -0800

    Add the KilogramToPound.py source file.

---
+++ Loops/KilogramToPound.py
@@ -0,0 +1,12 @@
+"""
+    This is a program that displays a conversion table that shows the conversion of weight from
+    kilograms to pounds.
+"""
+
+# Defines the conversion rate from kilograms to pounds.
+kilogramToPounds = 2.2
+
+# Displays the result in a tabular form.
+print("\nKilograms\t\tPounds")
+for i in range(1, 200, 2):  # For loop thats runs from 1 to 200 with a step of 2.
+    print(f"{i}\t\t{i * kilogramToPounds:14.2f}\t\t")
