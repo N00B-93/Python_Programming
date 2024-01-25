9a8f0a7a71e61b8a5b18973a93a99669a0561182
N00B-93 <omosheye007@gmail.com>
2024-01-25 07:35:39 -0800

    Add the OccurrenceOfMaxNumber.py source file.

---
+++ Loops/OccurrenceOfMaxNumber.py
@@ -0,0 +1,24 @@
+"""
+    This is a program that reads integers, finds the
+    largest of them, and counts its occurrences. Assume that the input ends with number 0.
+"""
+
+# Reads in a number
+number = int(input("\nEnter a number('0' to quit): "))
+
+# Initializes maxNumber with the first number and set the counter to 1.
+maxNumber = number
+countOfMax = 1
+
+
+while number != 0:
+    number = int(input("\nEnter a number('0' to quit): "))
+
+    # Updates the maxNumber and resets the counter to 1 if the current number is greater than the max number.
+    if number > maxNumber:
+        maxNumber = number
+        countOfMax = 1
+    elif number == maxNumber:  # Increments the counter by 1 if the current number is equal to the max number.
+        countOfMax += 1
+
+print(f"\nMax Number = {maxNumber}\nCount of Max Numbers = {countOfMax}")
