3fff8b6bca1708ae73e4d57074e993028b7fb234
N00B-93 <omosheye007@gmail.com>
2024-01-25 06:58:11 -0800

    Add the DecimalToBinary.py source file.

---
+++ Loops/DecimalToBinary.py
@@ -0,0 +1,25 @@
+"""
+    This is a program that prompts the user to enter a decimal number
+    and the displays the decimal number's binary equivalent.
+"""
+
+# Reads in a decimal number.
+number = int(input("\nEnter a decimal number: "))
+
+# Preserves the original decimal number in a temporary variable.
+tempVar = number
+
+binaryNumber = 0
+
+while number != 0:
+    # Determines the remainder
+    remainder = number % 2
+
+    # Constructs a binary number using the remainder.
+    binaryNumber = binaryNumber * 10 + remainder
+
+    # Peels off the last digit.
+    number //= 2
+
+# Displays the result
+print(f"\nThe binary equivalent of {tempVar} is: {binaryNumber}")
