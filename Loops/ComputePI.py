0915a123fe7bc7e4782dca7e98e02a6322efc610
N00B-93 <omosheye007@gmail.com>
2024-01-25 20:27:08 -0800

    Add the ComputePI.py source file.

---
+++ Loops/ComputePI.py
@@ -0,0 +1,21 @@
+from math import pow
+
+"""
+    This is a program that computes the value of PI using the formula:
+        PI = 4(1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + ... + (-1 ^(i + 1) / 2i - 1)
+    and displays the value of PI for i = 10000, 20000,  ..., 100000.
+"""
+
+# Initializes PI to 0
+PI = 0
+
+print("\ti\t\tPI")
+for i in range(10000, 100001):
+    for j in range(1, i):
+        # Calculates the value of PI from 1 to i.
+        PI += 4 * pow(-1, j + 1) / (2 * j - 1)
+    if i % 10000 == 0:
+        # Displays the value of PI if i is a multiple of 1.
+        print(f"{i}\t\t{PI}")
+    # Resets the value of PI to 0.
+    PI = 0
