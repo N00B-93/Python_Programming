07b656f8a5d619cfeedf889ba2b80a2cc38d2028
N00B-93 <omosheye007@gmail.com>
2024-01-25 21:32:29 -0800

    Add the PentagonalNumbers.py, SumOfDigits.py source files and the Functions directory.

---
+++ Functions/PentagonalNumbers.py
@@ -0,0 +1,34 @@
+"""
+    A Pentagonal number is a number defined as n(3n - 1)/2.
+    This is a program that uses a function with the header;
+            getPentagonalNumber(n) for n = 1, 2, 3,...
+    to calculate the first 100 pentagonal numbers and then the numbers are displayed them 10 per line.
+"""
+
+
+def getPentagonalNumber(number):
+    """
+    Returns a pentagonal number.
+    :param number: (int) The number whose pentagonal equivalent is to be calculated.
+    :return: (int) The pentagonal number
+    """
+
+    return number * (3 * number - 1) / 2
+
+
+def main():
+    lineCounter, n = 0, 1
+
+    while n < 100:
+        # Prints a pentagonal number followed by a new line.
+        print(f"{int(getPentagonalNumber(n))} ", end="")
+        n += 1  # Increments n by 1.
+        lineCounter += 1  # Increments lineCounter.
+        # Displays a newline if lineCounter is a multiple of 10.
+        if lineCounter % 10 == 0:
+            print()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ Functions/SumOfDigits.py
@@ -0,0 +1,36 @@
+"""
+    This is a program that uses a function with the header;
+            def sumDigits(n):
+    to calculate the sum of digits in a number.
+"""
+
+
+def sumDigits(number):
+    """
+
+    :param number: (int) The number whose sum of digits is to be calculated.
+    :return: (int) The sum of digits in a number
+    """
+
+    sumOfDigits = 0
+
+    while number != 0:
+        remainder = number % 10
+
+        sumOfDigits += remainder
+
+        number = number // 10
+
+    return sumOfDigits
+
+
+def main():
+    # Reads in a number.
+    number = int(input("\nEnter a number: "))
+
+    # Calculates and displays the sum of digits in the number.
+    print(f"\nThe sum of digits in {number} is: {sumDigits(number)}")
+
+
+if __name__ == "__main__":
+    main()
