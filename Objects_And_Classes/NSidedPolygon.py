4e982c0216a48d78691a852abce26b42dd8e2ab7
N00B-93 <omosheye007@gmail.com>
2024-02-02 09:07:58 +0100

    Add the NSidedPolygon.py source file.

---
+++ Objects_And_Classes/NSidedPolygon.py
@@ -0,0 +1,18 @@
+from math import tan, pi, pow
+
+
+class NSidedPolygon:
+    """
+    A class representing an n-sided polygon.
+
+    Attributes:
+        
+    """
+    def __init__(self, n = 1, side = 1, x = 0, y = 0):
+        self.__n = n
+        self.__side = side
+        self.__x = x
+        self.__y = y
+
+
+
