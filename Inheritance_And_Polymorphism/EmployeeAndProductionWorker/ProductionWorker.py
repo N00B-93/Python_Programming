69058ca2d872f2d34dc57dd21acdcaf87d714ffa
N00B-93 <omosheye007@gmail.com>
2024-02-19 15:00:11 -0800

    Add the ProductionWorker.py class file.

---
+++ Inheritance_And_Polymorphism/EmployeeAndProductionWorker/ProductionWorker.py
@@ -0,0 +1,60 @@
+from Employee import Employee
+
+
+class ProductionWorker(Employee):
+    """
+        A class Representing a Production Worker that inherits from Employee class.
+
+        Attributes:
+              shift (int): The shift of the employee.
+
+              hourlyPayRate (float): The hourly payment rate of the employee.
+    """
+    def __init__(self, shift, hourlyPayRate, name, ID):
+        super().__init__(name, ID)
+        self.__shift = shift
+        self.__hourlyPayRate = hourlyPayRate
+
+    def getShift(self):
+        """
+        Returns the shift of the employee.
+
+        :return: (int) The shift of the employee.
+        """
+        return self.__shift
+
+    def getHourlyPayRate(self):
+        """
+        Returns the hourly payment rate of the employee.
+
+        :return: (float) The hourly payment rate of the employee.
+        """
+        return self.__hourlyPayRate
+
+    def setShift(self, shift):
+        """
+        Sets the shift of the employee to the given shift.
+
+        :param shift: (int) The shift of the employee.
+
+        :return: (None)
+        """
+        self.__shift = shift
+
+    def setHourlyPayRate(self, hourlyPayRate):
+        """
+        Sets the hourly payment rate of the employee.
+
+        :param hourlyPayRate: (float) The hourly payment rate of the employee.
+
+        :return: (None).
+        """
+        self.__hourlyPayRate = hourlyPayRate
+
+    def __str__(self):
+        """
+        Returns the String representation of the Production Worker Object.
+
+        :return: (str) The string representation of the Production Worker Object.
+        """
+        return super().__str__() + "\nShift: " + str(self.__shift) + "\nHourly Pay: " + str(self.__hourlyPayRate)
