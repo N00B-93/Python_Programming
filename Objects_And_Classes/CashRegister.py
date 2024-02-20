b6cab3f5dda045d3eab86aa99e6276edce9e716a
N00B-93 <omosheye007@gmail.com>
2024-02-20 15:10:40 +0100

    Update the CashRegister.py source file.

--- Objects_And_Classes/CashRegister.py
+++ Objects_And_Classes/CashRegister.py
@@ -62,11 +62,51 @@ def main():
 
     item3 = RetailItem("Leather Jacket", 11, 45.60)
 
-    # Creates a list of items.
-    itemList = [item1, item2, item3]
+    item4 = RetailItem("Baseball Cap", 10, 9.12)
 
+    item5 = RetailItem("Nike Air Jordan", 8, 101.28)
+
+    # Creates a list to represent an empty cart.
+    cart = []
+    
+    choice = ""
+
+    while choice != '0':
+        print("\n\t\tAvailable Items")
+        
+        print(f"1. {item1.getDescription()}\n2. {item2.getDescription()}\n3. {item3.getDescription()}\n4. {item4.getDescription()}\n5. {item5.getDescription()}")
+        choice = input("\nEnter choice(press 0 to quit): ")
+
+        match choice:
+            case '1':
+                cart.append(item1)
+            case '2':
+                cart.append(item2)
+            case '3':
+                cart.append(item3)
+            case '4':
+                cart.append(item4)
+            case '5':
+                cart.append(item5)
+            case _:
+                print("\nInvalid option!")
+    
     # Creates a CashRegister Object.
-    cashRegister = CashRegister(itemList)
+    cashRegister = CashRegister(cart)
+
+    # Displays the items in the cart.
+    print("\n\t\tItems in Cart:")
+    print("\nItem Name\t\t\tQty")
+    for items in cart:
+        print(f"\n{items.getDescription()}\t\t\t{cart.count(items)}")
+    
+    # Displays the total price of all items in the cart.
+    total = 0
+
+    for item in cart:
+        total += item.getPrice()
+
+    print(f"\nThe total price of all items in the cart is: $ {total:.2f}")
 
 
 if __name__ == "__main__":
