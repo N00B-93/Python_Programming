b49572e549bc88e162b09e2eac91399761bbbc07
N00B-93 <omosheye007@gmail.com>
2024-03-01 01:27:40 +0100

    Add the Files_And_Exception_Handling directory and the RemoveText.py source file.

---
+++ Files_And_Exception_Handling/RemoveText.py
@@ -0,0 +1,33 @@
+"""
+    This is a program that prompts the user to enter a file name and a String to be removed, then removes
+    all occurrences of the specified String from the file.
+"""
+
+
+def main() -> None:
+    # Prompts the user to enter the name of the file and the String to be removed.
+    fileName = input("\nEnter the name of the file: ")
+    string = input("\nEnter the String to be removed from the file: ")
+
+    try:
+        fileContent, fileHandler  = '', None
+        # Reads the content of the file into a String variable.
+        with open(fileName, 'r') as fileHandler:
+            fileContent = fileHandler.read()
+        
+        # Replace all occurrence of the String with an empty String.
+        fileContent = fileContent.replace(string, '')
+        
+        # Write the file content back to the file without the specified String.
+        with open(fileName, 'w') as fileHandler:
+            fileHandler.write(fileContent)
+        print(f"\n{string} removed from the {fileName} file!")
+    except IOError as ioError:
+        print(ioError)
+    except Exception as exception:
+        print(exception)
+
+
+if __name__ == "__main__":
+    main()
+
