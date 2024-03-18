74d6abef41cb0b09cb14adc54b10485b97e7905b
N00B-93 <omosheye007@gmail.com>
2024-03-18 12:20:41 -0700

    Add the FibonacciWithoutRecursion.py source file.

---
+++ Recursion/FibonacciWithoutRecursion.py
@@ -0,0 +1,40 @@
+"""
+    This is a program that prompts the user to enter an index and then displays the fibonacci number that
+    corresponds to that index.
+"""
+
+
+def fibonacciWithoutRecursion(index: int) -> int:
+    """
+    This returns the Fibonacci number at a particular index.
+    :param index: (int) The index of the Fibonacci number
+    :return: (int) The Fibonacci number at the given index.
+    """
+    previous = 0
+    current = 1
+
+    for i in range(2, index + 1):
+        nextTerm = previous + current
+        previous = current
+        current = nextTerm
+    return current
+
+
+def main() -> None:
+    while True:
+        try:
+            # Prompts the user to enter the index of the Fibonacci number to be displayed.
+            number: int = int(input("\nEnter an index > 0 for a Fibonacci number: "))
+
+            # Displays 0 if index entered by user is 0.
+            if number == 0:
+                print(f"\nThe Fibonacci number at index {number} is: 0")
+            else:
+                # Displays the Fibonacci number at the index supplied by the user.
+                print(f"\nThe Fibonacci number at index {number} is: {fibonacciWithoutRecursion(number)}")
+        except ValueError:
+            print("\nInvalid input. Try again.")
+
+
+if __name__ == "__main__":
+    main()
