6edefa009abbbf584ab467bd02922874ae329664
N00B-93 <omosheye007@gmail.com>
2024-04-21 20:11:52 +0100

    Add the CompareCost.py source file.

--- Selections/CompareCost.py
+++ Selections/CompareCost.py
@@ -1,7 +1,6 @@
 """
-    This is a program that prompts the user to enter the weight and price
-    of two different packages of rice , then compares the cost of two packages and displays the
-    one with the better price.
+    This is a program that prompts the user to enter the weight and price of two different packages of rice, then 
+    compares the cost of two packages and displays the one with the better price.
 """
 
 # Reads in the weight and price of each package.
@@ -15,5 +14,7 @@ pricePerKg2 = price2 / weight2
 # Displays the package with the better price based on the pricePerKg.
 if pricePerKg1 < pricePerKg2:
     print("\nPackage1 has the better price.")
-else:
+elif pricePerKg1 > pricePerKg2:
     print("\nPackage2 has the better price.")
+else:
+    print("\nBoth packages have the same price.")
