6ed46e32acf7f9c44434d0fcc98c82978cd0da40
N00B-93 <omosheye007@gmail.com>
2024-05-03 13:55:54 +0100

    Add the CalculateInterest.py source file.

--- Elementary_Programming/CalculateInterest.py
+++ Elementary_Programming/CalculateInterest.py
@@ -1,1 +1,16 @@
+"""
+    This is a program that prompts the user to enter the principal
+    and the annual interest rate and then displays the displays the monthly interest.
+"""
 
+# Prompts the user to enter the principal.
+principal = float(input("\nEnter the principal: $"))
+
+# Prompts the user to enter the annual interest rate.
+interestRate = float(input("\nEnter the annual interest rate(e.g. 3%): "))
+
+# Computes the monthly interest.
+monthlyInterest = principal * interestRate / 1200;
+
+# Displays the result.
+print(f"\nThe interest for the next month is: ${monthlyInterest:.2f}")
