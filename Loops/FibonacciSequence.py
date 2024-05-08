528750bc07cfdba7d0ec4ad51ff4771c7bdf574f
N00B-93 <omosheye007@gmail.com>
2024-05-08 16:37:10 +0100

    Add the fibonacciSequence.py source file.

--- Loops/FibonacciSequence.py
+++ Loops/FibonacciSequence.py
@@ -5,11 +5,25 @@
 """
 
 # Prompts the user to enter a number.
-number = input("\nEnter a number representing the nth term of the Fibonacci sequence: ")
+number = input("\nEnter a number for n = 0, 1, 2, ...: ")
 
 # Displays an error message and continue to prompt the user to enter a correct input if user input is invalid.
-while int(number) < 0:
+while not number.isdigit() or int(number) < 0:
     print(f"\nError: Use a number greater than 0, Try again.")
     number = input("\nEnter a number representing the nth term of the Fibonacci sequence: ")
 
+previousTerm, currentTerm, nextTerm = 0, 1, 0
 
+# Displays the Fibonacci number at the 0th or 1st index respectively depending on the user's input.
+if int(number) == 0 or int(number) == 1:
+    print(f"\nThe Fibonacci number at index {number} is: {number}\n")
+    exit(0)
+
+# Determines the Fibonacci number at the nth index.
+for i in range(int(number) - 1):
+    nextTerm = previousTerm + currentTerm
+    previousTerm = currentTerm
+    currentTerm = nextTerm
+
+# Displays the Fibonacci number at the index entered by the user.
+print(f"\nThe Fibonacci number at index {number} is: {nextTerm}\n")
