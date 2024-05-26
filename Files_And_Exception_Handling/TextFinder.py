7a3c0470e76a89b40750a7f67fe808831f7ba577
N00B-93 <omosheye007@gmail.com>
2024-05-26 22:42:45 +0100

    Add the TextFinder.py source file.

---
+++ Files_And_Exception_Handling/TextFinder.py
@@ -0,0 +1,34 @@
+from sys import argv
+
+"""
+    This is a program that searches all files specified on the command line and 
+    prints out all lines containing a specified word. The specified word is always the first command line argument. 
+"""
+
+
+def main() -> None:
+    # checks if the user uses valid command line arguments.
+    if len(argv) < 3:
+        print("\nUsage: TextFinder.py textToFind FileToSearch1 FileToSearch2 FileToSearch3 ...")
+        exit(1)
+
+    # List to hold all occurrence of the word in the files.
+    textAndFilename: list = []
+
+    try:
+        for i in range(2, len(argv)):  # Loops through the files to search for the occurrence of the specified word.
+            with open(argv[i]) as fileHandler:
+                for line in fileHandler:
+                    if line.__contains__(argv[1]):  # Appends a line of text to the textAndFilename list if it contains
+                        # the specified word.
+                        textAndFilename.append(fileHandler.name + ": " + line)
+    except Exception as exception:
+        print(exception.__str__())
+
+    # Displays all occurrence of the word in the files.
+    for item in textAndFilename:
+        print(item)
+
+
+if __name__ == "__main__":
+    main()
