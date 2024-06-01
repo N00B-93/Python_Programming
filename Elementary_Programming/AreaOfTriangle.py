66306504cbb21350e79861ece6e1ed153a35d1f2
N00B-93 <omosheye007@gmail.com>
2024-06-01 16:54:51 +0100

    Format the print statement displaying the area to diplay area to 2 decimal places.

--- Elementary_Programming/AreaOfTriangle.py
+++ Elementary_Programming/AreaOfTriangle.py
@@ -22,4 +22,4 @@ perimeter = (side1 + side2 + side3) / 2
 area = sqrt(perimeter * (perimeter - side1) * (perimeter - side2) * (perimeter - side3))
 
 # Displays the result.
-print(f"\nThe area of the triangle is: {area}")
+print(f"\nThe area of the triangle is: {area:.2f}")
