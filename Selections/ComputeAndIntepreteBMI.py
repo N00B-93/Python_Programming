a8fa9172696a23c708218b7b53537cb64d5f4710
N00B-93 <omosheye007@gmail.com>
2024-06-03 12:56:57 +0100

    Include if block to verify user input and also add a statement to display the BMI value and properly format the statement displaying the user's health status.

--- Selections/ComputeAndIntepreteBMI.py
+++ Selections/ComputeAndIntepreteBMI.py
@@ -13,10 +13,21 @@ KILOGRAMS_PER_POUND = 0.45359237
 METERS_PER_INCH = 0.0254
 FEET_TO_METERS = 0.305
 
-# Prompts the user to enter weight in pounds and height in feet and inches.
+# Prompts the user to enter weight in pounds.
 weight = float(input("\nEnter your weight in pounds: "))
-feet = float(input("\nEnter feet: "))
-inches = float(input("\nEnter inches: "))
+
+# Displays an error message and terminates the program if the weight is <= 0.
+if weight <= 0:
+    print(f"\nError: Use a weight > 0, Try again.\n")
+    exit(1)
+
+# Prompts the user to enter the value for the feet and inches.
+feet, inches = eval(input("\nEnter feet and inches separated by comma: "))
+
+# Displays an error message and terminates the program if the feet or inches is <= 0.
+if feet <= 0 or inches <= 0:
+    print(f"\nError: Use positive values > 0 for feet and inches, Try again.\n")
+    exit(2)
 
 # Converts weight from pounds to kg and height from feet and inches to meters.
 weightInKg = weight * KILOGRAMS_PER_POUND
@@ -30,11 +41,12 @@ height = heightInMeters + inchesToMeters
 bmi = weightInKg / (pow(height, 2))
 
 # Displays the user's BMI Status based on the calculated BMI value.
+print(f"\nBMI: {bmi:.2f}")
 if bmi < 18.5:
-    print("\nUnderweight")
+    print("You are Underweight")
 elif 18.5 <= bmi <= 24.9:
-    print("\nNormal")
+    print("You are Normal")
 elif 24.9 < bmi <= 29.9:
-    print("\nOverweight")
+    print("You are Overweight")
 elif 30 > bmi:
-    print("\nObese")
+    print("You are Obese")
