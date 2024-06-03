43b1c0d67da12cbf1a3b5955647840e9ee73a7e3
N00B-93 <omosheye007@gmail.com>
2024-06-03 12:58:23 +0100

    Include if blocks to check whether the weight and price entered by the user is valid.

--- Selections/CompareCost.py
+++ Selections/CompareCost.py
@@ -3,9 +3,21 @@
     compares the cost of two packages and displays the one with the better price.
 """
 
-# Reads in the weight and price of each package.
+# Reads in the weight and price of the first package.
 weight1, price1 = eval(input("\nEnter the weight and price of the first package separated by comma: "))
+
+# Displays an error message and exits the program if the user enters an invalid weight or price.
+if weight1 <= 0 or price1 <= 0:
+    print("\nError: weight and price should be > 0, Try again.")
+    exit(1)
+
+# Reads in the weight and price of the second package.
 weight2, price2 = eval(input("\nEnter the weight and price of the second package separated by comma: "))
+
+# Displays an error message and exits the program if the user enters an invalid weight or price.
+if weight2 <= 0 or price2 <= 0:
+    print("\nError: weight and price should be > 0, Try again.")
+    exit(2)
 
 # Calculates the pricePerKg of each package.
 pricePerKg1 = price1 / weight1
