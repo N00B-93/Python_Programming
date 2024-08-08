4e29834184199c8aebaf8bb62741deb29b747f0c
N00B-93 <omosheye007@gmail.com>
2024-08-08 15:52:29 +0100

    Remove magic numbers from code
    Used variables to hold the conversion factors used to convert
    weight from pounds to kilograms and inches to meters.

--- Elementary_Programming/CalculateBMI.py
+++ Elementary_Programming/CalculateBMI.py
@@ -11,8 +11,12 @@ weight = float(input("\nEnter your weight in pounds: "))
 # Reads in the height.
 height = float(input("\nEnter your height in inches: "))
 
+# Constants used to convert weight from lbs to kg and length from in to m.
+POUNDS_TO_KILOGRAM = 0.45359237
+INCHES_TO_METERS = 0.0254
+
 # Calculates the BMI by converting the weight from pounds to kg and the height from inches to meters.
-bmi = (weight * 0.45359237) / (pow(height * 0.0254, 2))
+bmi = (weight * POUNDS_TO_KILOGRAM) / (pow(height * INCHES_TO_METERS, 2))
 
 # Displays the result.
 print(f"\nBMI is {bmi:.2f}")
