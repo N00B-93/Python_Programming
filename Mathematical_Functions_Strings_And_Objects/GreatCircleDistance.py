6181e1d57a3ea6644f76f873a004be4f891db3a9
N00B-93 <omosheye007@gmail.com>
2024-08-09 22:51:15 +0100

    Store Earth's radius in a constant
    Store the value of the Earth's radius in a constant and use it to calculate the great circle distance instead of using a magic number.

--- Mathematical_Functions_Strings_And_Objects/GreatCircleDistance.py
+++ Mathematical_Functions_Strings_And_Objects/GreatCircleDistance.py
@@ -1,10 +1,14 @@
 from math import sin, cos, acos, radians
+from typing import Final
 
 """
     This is a program that prompts the user to enter the latitude and longitude of two
     points on the earth in degrees and displays its great circle distance. The average
     earth radius is 6,371.01 km.
 """
+
+# Constant holding the value of the Earth's radius.
+RADIUS_OF_EARTH: Final[float] = 6371.01
 
 # Reads in the Latitude and Longitude for point1 and point2 respectively.
 lat1, lon1 = eval(input("\nEnter point 1 (latitude and longitude) in degrees separated by comma: "))
@@ -15,7 +19,7 @@ lat1, lat2 = radians(lat1), radians(lat2)
 lon1, lon2 = radians(lon2), radians(lon1)
 
 # Calculates the great circle distance.
-greatCircleDistance = 6371.01 * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon1 - lon2))
+greatCircleDistance = RADIUS_OF_EARTH * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon1 - lon2))
 
 # Displays the result.
 print(f"\nThe distance between two points is {greatCircleDistance:.2f} km")
