1884bbaff23f8fdec063e3617fe4a584d13bac1b
N00B-93 <omosheye007@gmail.com>
2024-08-13 01:30:00 +0100

    Remove unnecessary semi colon from code
     Remove unnecessary semi colon from code and correct spelling error in
     code comment.

--- Elementary_Programming/CalculateInterest.py
+++ Elementary_Programming/CalculateInterest.py
@@ -1,6 +1,6 @@
 """
     This is a program that prompts the user to enter the principal
-    and the annual interest rate and then displays the displays the monthly interest.
+    and the annual interest rate and then displays the monthly interest.
 """
 
 # Prompts the user to enter the principal.
@@ -10,7 +10,7 @@ principal = float(input("\nEnter the principal: $"))
 interestRate = float(input("\nEnter the annual interest rate(e.g. 3%): "))
 
 # Computes the monthly interest.
-monthlyInterest = principal * interestRate / 1200;
+monthlyInterest = principal * interestRate / 1200
 
 # Displays the result.
 print(f"\nThe interest for the next month is: ${monthlyInterest:.2f}")
