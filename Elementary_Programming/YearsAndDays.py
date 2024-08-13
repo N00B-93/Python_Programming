fdc116c66422bbede06525f11af4e8f835fea2ab
N00B-93 <omosheye007@gmail.com>
2024-08-13 01:05:55 +0100

    Use constants instead of integer literals
    Use constant to hold the number of minutes in a year and the number of
    minutes in a day and then use these constants to determines the number
    of years and days instead of just using integer literals.

--- Elementary_Programming/YearsAndDays.py
+++ Elementary_Programming/YearsAndDays.py
@@ -6,11 +6,17 @@
 # Reads in the minute.
 minutes = int(input("\nEnter the number of minutes: "))
 
+# Constant to hold the number of minutes in a year.
+MINUTES_IN_A_YEAR = 525600
+
 # Calculates the years.
-years = minutes // 525600
+years = minutes // MINUTES_IN_A_YEAR
+
+# Constant to hold the number of minutes in a day.
+MINUTES_IN_A_DAY = 1440
 
 # Calculates the days.
-days = (minutes % 525600) // 1440
+days = (minutes % 525600) // MINUTES_IN_A_DAY
 
 # Displays the result.
 print(f"\n{minutes} minutes is approximately {years} years and {days} days")
