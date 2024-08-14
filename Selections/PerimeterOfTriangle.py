e48512705b77f246dcd3bc5b0bad7db9dece1469
N00B-93 <omosheye007@gmail.com>
2024-08-14 19:58:13 +0100

    Make changes to code documentation

--- Selections/PerimeterOfTriangle.py
+++ Selections/PerimeterOfTriangle.py
@@ -1,6 +1,6 @@
 """
-    This is a program that reads three edges for a triangle and computes the perimeter if the input is valid. 
-    Otherwise, display that the input is invalid. The input is valid if the sum of every pair of two edges is greater
+    This is a program that reads three sides for a triangle and computes the perimeter if the input is valid.
+    Otherwise, display that the input is invalid. The input is valid if the sum of every pair of two sides is greater
     than the remaining edge.
 """
 
