4485cee57e3121cf665b7a2398bd9bcdeb0f8b79
N00B-93 <omosheye007@gmail.com>
2024-09-02 22:41:14 +0100

    Correct error in variable name

--- Selections/LearnMultiplication.py
+++ Selections/LearnMultiplication.py
@@ -11,13 +11,13 @@ number1 = randint(1, 100)
 number2 = randint(1, 100)
 
 # Calculates the sum of the numbers
-sumOfNumbers = number1 * number2
+productOfNumbers = number1 * number2
 
 # Prompts the user to enter the sum of the numbers.
 answer = int(input(f"\nWhat is {number1} * {number2}? "))
 
 # Informs the user if the solution he/she enters is right or wrong.
-if answer == sumOfNumbers:
+if answer == productOfNumbers:
     print("\nCorrect!")
 else:
     print("\nWrong!")
