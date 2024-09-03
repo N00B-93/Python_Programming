ea283e949356c2bf9e0904fc14d4e7d3ae21f3c7
N00B-93 <omosheye007@gmail.com>
2024-09-03 08:26:53 +0100

    Modularize code using a function
    Add a function to determine whether two alphabetic Strings are anagrams and
    also add code to validate user input.

--- Lists/Anagram.py
+++ Lists/Anagram.py
@@ -8,26 +8,43 @@ from sys import exit
                 def isAnagram(s1, s2)
 """
 
+def isAnagram(s1, s2):
+    """
+    Determines whether two Strings are anagrams.
+
+    Parameters:
+        s1 (str): The first String.
+
+        s2 (str): The second String.
+
+    Returns:
+        bool: True if the Strings are anagrams, else False.
+    """
+    if len(s1) != len(s2):
+        return False
+
+    lst1, lst2 = list(s1.lower()), list(s2.lower())
+
+    lst1.sort()
+    lst2.sort()
+
+    return lst1 == lst2
+
 
 def main():
     # Prompts the user to enter two Strings to be compared.
     string1 = input("\nEnter the first string: ").strip()
     string2 = input("\nEnter the second string: ").strip()
-    
-    # Terminates the program if the Strings have unequal lengths.
-    if len(string1) != len(string2):
-        print(f"\n{string1} and {string2} are not anagrams.")
+    # Validate user input.
+    if not string1 or not string2:
+        print("\nError: Both Strings must be non-empty Strings only, Try again")
         exit(1)
+    elif not string1.isalpha() or not string2.isalpha():
+        print("\nError: Both Strings must be alphabetic, Try again")
+        exit(2)
 
-    # Converts the Strings to lists.
-    lst1, lst2 = list(string1), list(string2)
-    
-    # Sorts the two lists.
-    lst1.sort()
-    lst2.sort()
-
-    # Compares the sorted lists and displays the result.
-    if lst1 == lst2:
+    # Displays the result.
+    if isAnagram(string1, string2):
         print(f"\n{string1} and {string2} are anagrams.")
     else:
         print(f"\n{string1} and {string2} are not anagrams.")
